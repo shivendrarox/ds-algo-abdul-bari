1195308bed85cdf76b3ca12884181ea1ddd95efc
shivendrarox <12queenvictoria12@gmail.com>
2021-01-10 22:28:01 +0530

    added structurers.cpp, it shows that size of of a card struct is 12 bytes

--- structures.cpp
+++ structures.cpp
@@ -10,5 +10,5 @@ int main(){
 	struct card deck[52] = {{1,0,0},{2,0,0},{1,0,0}};
 	printf("%d\n",deck[0].face);
 	printf("%d\n",deck[0].shape);
-	printf("%d\n",sizeof(deck[0].color));
+	printf("%d\n",sizeof(deck[0]));
 }
